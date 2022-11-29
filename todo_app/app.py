ecf34b632be00c1a9b38d7c6995ab6ad3b07ef1d
Cristi Neagu <cristi.m.neagu@gmail.com>
2022-11-29 19:25:49 +0000

    Refactored code to have just one single form

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask import request
 
 from todo_app.flask_config import Config
@@ -12,16 +12,54 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     '''Main page'''
-    return render_template(r'index.html', items=session_items.get_items())
+    return render_template('index.html', items=session_items.get_items())
 
-@app.route('/addItem', methods=['POST'])
-def addItem():
-    '''Add a new item to the list'''
-    session_items.add_item(request.form.get('title'))
-    return redirect(url_for('index'))
+@app.route('/changeItem', methods=['POST'])
+def changeItem():
+    '''Can create, edit, and delete an item'''
+    reqAction = request.form.get('changeItem')
 
-@app.route('/removeItem', methods=['post'])
-def removeItem():
-    '''Remove an item based on ID'''
-    session_items.delete_item(request.form.get('itemID'))
+    if reqAction == 'Add Item':
+        itemTitle = request.form.get('itemTitle')
+        if itemTitle == "":
+            flash('ERROR: No title specified for new item')
+        else:
+            session_items.add_item(itemTitle)
+
+    if reqAction == 'Delete Item':
+        itemID = request.form.get('itemID')
+        if itemID == "":
+            flash('ERROR: No ID specified for item to be deleted')
+        else:
+            session_items.delete_item(itemID)
+
+    if reqAction == 'Get Item':
+        itemID = request.form.get('itemID')
+        if itemID == "":
+            flash('ERROR: No ID specified for item to be retrieved')
+        else:
+            item = session_items.get_item(itemID)
+            itemStart = item['status'] == 'Started'
+            itemTitle = item['title']
+            return render_template('index.html', items=session_items.get_items(),
+                                    setID=itemID, setStart=itemStart, setTitle=itemTitle)
+
+    if reqAction == "Save Item":
+        itemID = request.form.get('itemID')
+        if itemID == "":
+            itemTitle = request.form.get('itemTitle')
+            session_items.add_item(itemTitle)
+        else:
+            items = session_items.get_items()
+            itemStart = request.form.get('isStarted') is not None
+            itemTitle = request.form.get('itemTitle')
+            for item in items:
+                if item['id'] == int(itemID):
+                    item['title'] = itemTitle
+                    item['status'] = 'Started' if itemStart else 'Not started'
+                    session_items.save_item(item)
+                    break
+            else:
+                session_items.add_item(itemTitle)
+
     return redirect(url_for('index'))
