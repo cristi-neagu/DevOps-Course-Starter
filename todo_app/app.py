da22add3478adc2afe6f271177ca498d7b81d85b
Cristi Neagu <cristi.m.neagu@gmail.com>
2022-11-29 14:41:23 +0000

    Can now add items, but it doesn't look quite right

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 from todo_app.flask_config import Config
 import todo_app.data.session_items as session_items
@@ -10,4 +11,11 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
+    '''Main page'''
+    return render_template(r'index.html', items=session_items.get_items())
+
+@app.route('/addItem', methods=['POST'])
+def addItem():
+    '''Add a new item to the list'''
+    session_items.add_item(request.form.get('title'))
     return render_template(r'index.html', items=session_items.get_items())
