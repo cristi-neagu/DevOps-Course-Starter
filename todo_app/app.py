48150939e646a2ae3a536018f284b8546490cdd9
Cristi Neagu <cristi.m.neagu@gmail.com>
2022-11-29 14:13:38 +0000

    Modify index.html to show items

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 
 from todo_app.flask_config import Config
+import todo_app.data.session_items as session_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,4 +10,4 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template(r'index.html')
+    return render_template(r'index.html', items=session_items.get_items())
