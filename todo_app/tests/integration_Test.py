87a4a958d0873fe929e10da8dbc1bec7d1819feb
Cristi Neagu <cristi.m.neagu@gmail.com>
2023-08-16 03:15:37 +0100

    Major bugfix, got the monkeypatch actually working

--- todo_app/tests/integration_Test.py
+++ todo_app/tests/integration_Test.py
@@ -1,40 +1,45 @@
 import pytest
 import requests
 import os
+import json
 from dotenv import load_dotenv, find_dotenv
 from todo_app import app
 
 @pytest.fixture
 def client():
     # Use our test integration config instead of the 'real' version
-    file_path = find_dotenv('.env.test')
-    load_dotenv(file_path, override=True)
+    filePath = find_dotenv('.env.testing')
+    load_dotenv(filePath, override=True)
 
     # Create the new app.
     testApp = app.create_app()
 
     # Use the app to create a test_client that can be used in our tests.
-    with testApp.test_client() as testClient:
-        yield testClient
+    with testApp.test_client() as client:
+        yield client
 
 def testIndexPage(monkeypatch, client):
-    monkeypatch.setattr(requests, 'get', stub)
+    monkeypatch.setattr(requests, 'request', stub)
     response = client.get('/')
+    assert response.status_code == 200
 
 class StubResponse():
-    def __init__(self, fake_response_data):
-        self.fake_response_data = fake_response_data
+    def __init__(self, fakeResponseData):
+        self.fakeResponseData = fakeResponseData
+        self.text = json.dumps(self.fakeResponseData)
 
     def json(self):
-        return self.fake_response_data
+        return self.fakeResponseData
 
-def stub(url, params={}):
-    test_board_id = os.environ.get('TRELLO_BOARD_ID')
-    if url == f'https://api.trello.com/1/boards/{test_board_id}/lists':
-        fake_response_data = [{
+def stub(reqType, url, **kwargs):
+    # Not sure why this isn't working...
+    # test_board_id = os.environ.get('TRELLO_BOARD_ID')
+    testBoardID = 'TestTrelloBoard'
+    if url == f'https://api.trello.com/1/boards/{testBoardID}/lists':
+        fakeResponseData = [{
             'id': '123abc',
             'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card'}]
+            'cards': [{'id': '456', 'name': 'Test card', 'idShort': '0'}]
             }]
-        return StubResponse(fake_response_data)
+        return StubResponse(fakeResponseData)
     raise Exception(f'Integration test did not expect URL "{url}"')
