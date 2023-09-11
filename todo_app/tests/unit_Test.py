3290aec1cc74cb57ef4e2d68b594707a6fb49e0c
Cristi Neagu <cristi.m.neagu@gmail.com>
2023-09-11 14:08:53 +0100

    Moved test items to a fixture for code brevity

--- todo_app/tests/unit_Test.py
+++ todo_app/tests/unit_Test.py
@@ -2,8 +2,9 @@ import pytest
 from todo_app.data.ViewModel import ViewModel
 from todo_app.data.ToDoItem import TDItem
 
-def testDoing():
-    testItems = [
+@pytest.fixture
+def testItems():
+    return [
         TDItem(1, '1st item', 'Doing'),
         TDItem(2, '2nd item', 'Doing'),
         TDItem(3, '3rd item', 'To-Do'),
@@ -16,48 +17,16 @@ def testDoing():
         TDItem(10, '10th item', 'To-Do'),
         TDItem(11, '11th item', 'Done'),
         TDItem(12, '12th item', 'Done')
-        ]
+    ]
 
+def testDoing(testItems):
     itemModel = ViewModel(testItems)
-
     assert len(itemModel.doingItems) == 4
 
-def testDone():
-    testItems = [
-        TDItem(1, '1st item', 'Doing'),
-        TDItem(2, '2nd item', 'Doing'),
-        TDItem(3, '3rd item', 'To-Do'),
-        TDItem(4, '4th item', 'Doing'),
-        TDItem(5, '5th item', 'Done'),
-        TDItem(6, '6th item', 'Doing'),
-        TDItem(7, '7th item', 'Done'),
-        TDItem(8, '8th item', 'Done'),
-        TDItem(9, '9th item', 'To-Do'),
-        TDItem(10, '10th item', 'To-Do'),
-        TDItem(11, '11th item', 'Done'),
-        TDItem(12, '12th item', 'Done')
-        ]
-
+def testDone(testItems):
     itemModel = ViewModel(testItems)
-
     assert len(itemModel.doneItems) == 5
 
-def testToDo():
-    testItems = [
-        TDItem(1, '1st item', 'Doing'),
-        TDItem(2, '2nd item', 'Doing'),
-        TDItem(3, '3rd item', 'To-Do'),
-        TDItem(4, '4th item', 'Doing'),
-        TDItem(5, '5th item', 'Done'),
-        TDItem(6, '6th item', 'Doing'),
-        TDItem(7, '7th item', 'Done'),
-        TDItem(8, '8th item', 'Done'),
-        TDItem(9, '9th item', 'To-Do'),
-        TDItem(10, '10th item', 'To-Do'),
-        TDItem(11, '11th item', 'Done'),
-        TDItem(12, '12th item', 'Done')
-        ]
-
+def testToDo(testItems):
     itemModel = ViewModel(testItems)
-
     assert len(itemModel.toDoItems) == 3
